5e54c8fc90d0179a84a414f83246d1b9d0505ee0
Cameron Trando <cameron.trando@yahoo.com>
2019-06-13 16:00:57 -0700

    Adding text to each component to say their time ranges in human readable format

--- src/calendar/event/Event.js
+++ src/calendar/event/Event.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import {format} from 'date-fns';
 
 export class Event extends React.PureComponent {
 
@@ -9,14 +10,21 @@ export class Event extends React.PureComponent {
             editing: false
         }
     }
+
     /**
      * Returns a renderable react component
      */
     render() {
+        const startFormatted = format(this.props.start, 'h:mma');
+        const endFormatted = format(this.props.end, 'h:mma');
+
+        const formatStr = `${startFormatted}-${endFormatted}`;
         return (
-            <div onDoubleClick={(e) => this.setState({editing: true})} style={{height: "100%", backgroundColor: "grey"}}>
+            <div onDoubleClick={(e) => this.setState({editing: true})}
+                 style={{height: "100%", backgroundColor: "grey"}}>
                 {this.state.editing && <div>hello </div>}
-                how are you
+                <div>how are you</div>
+                <div>{formatStr}</div>
             </div>
         )
     }
